9b4bda7c8d41c7acd10ac08f1b57533b7ee67712
YamanoItsuki <42466955+ituyama@users.noreply.github.com>
2022-11-19 03:00:37 +0900

    add code

--- js/index.js
+++ js/index.js
@@ -176,53 +176,53 @@ const menu = [{
         name: "納豆",
         price: 90
     },
-        {
+    {
         name: "ごはん",
         price: 160
     },
 
-        {
+    {
         name: "フライドにんにく",
         price: 60
     },
 
-        {
+    {
         name: "キムチ",
         price: 150
     },
 
-        {
+    {
         name: "山かけ(わさび付)",
         price: 150
     },
 
-        {
+    {
         name: "おろしポン酢",
         price: 150
     },
 
-        {
+    {
         name: "高菜明太マヨ",
         price: 150
     },
 
-        {
+    {
         name: "高菜",
         price: 120
     },
 
-        {
+    {
         name: "明太マヨ",
         price: 40
     },
 
 
-        {
+    {
         name: "青ねぎ",
         price: 80
     },
 
-        {
+    {
         name: "のり",
         price: 30
     },
@@ -232,73 +232,113 @@ const menu = [{
     },
 
 
-        {
+    {
         name: "ゴマだれ",
         price: 50
     },
 
 
 
-       {
+    {
         name: "旨辛ダレ",
         price: 40
     },
 
-              {
+    {
         name: "コチュジャンだれ",
         price: 40
     }
 
 
 ]
+
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
 function ChoiceGyudon() {
-    return menu[getRandomInt(0,21)]
+    return menu[getRandomInt(0, 21)]
 }
+
 function ChoiceRandom() {
-    fs=ChoiceGyudon()
-    var price=fs.price
+    fs = ChoiceGyudon()
+    var price = fs.price
     var MenuList = [fs.name]
-    var MenuPriceList= [fs.price]
-    var res 
-    var TotalList=[fs]
-    
+    var MenuPriceList = [fs.price]
+    var res
+    var TotalList = [fs]
+
     while (price < 1000) {
-   
+
         MenuNow = menu[getRandomInt(22, 56)]
-   
+
         MenuList[MenuList.length] = MenuNow.name
         MenuPriceList[MenuPriceList.length] = MenuNow.price
         TotalList[TotalList.length] = MenuNow
-       
-     
+ 
         price = price + MenuNow.price
-    
+  
         res = [MenuList, MenuPriceList]
-        
+
     }
+
+
     if (price > 1000) {
-            res[0].pop();
+        res[0].pop();
         res[1].pop();
         TotalList.pop()
-         
+
 
     }
-    res[2] = MenuPriceList.reduce(function (a, b) { return a + b; });
-  
 
-//
-      
+    res[2] = MenuPriceList.reduce(function (a, b) {
+        return a + b;
+    });
 
-    TotalList[TotalList.length] = { Total:res[2]}
+
+    //
+
+
+    TotalList[TotalList.length] = {
+        Total: res[2]
+    }
     return TotalList
 
 }
 
+console.log("答え", ChoiceRandom().length)
 
-console.log("答え",ChoiceRandom())
+function AddHTML() {
+    ins = ChoiceRandom();
+    while(ins[ins.length - 1].Total < 970) {
+        ins = ChoiceRandom();
+    }
+
+    let strs = "";
+    let share_url = "";
+    for (let i = 0; i < ins.length - 1; i++){
+        strs = strs + "<div class='col-md-6'><div class='card'><p class='left'>" + ins[i].name + "</p><p class='prices right'>" + ins[i].price + "円</p></div></div>";
+        share_url = share_url+ins[i].name+"、"+ins[i].price+"円\n";
+    }
+    strs = strs + "<div class='col-md-6'><div class='card'><p class='left'>合計" + ins[ins.length-1].Total +"円</p></div></div>"
+    document.getElementById('content').innerHTML = strs;
+    share_url = "合計"+share_url + ins[ins.length - 1].Total + "円";
+    console.log(strs)
+    ShareTwitter(share_url)
+}
+
+
+// シェアボタン追加
+
+function ShareTwitter(share) {
+var shareUrl  = 'https://twitter.com/intent/tweet';
+shareUrl += '?text='+encodeURIComponent("すき家1000円ガチャを回したよ!\n"+share);
+shareUrl += '&url='+encodeURIComponent('\nhttps://ituyama.com/SukiyaRandom\n');
+shareUrl+='&hashtags=すき家1000円ガチャ'
+    var shareArea = document.getElementById('twitter-share');
+    
+var shareLink = "<a target='_blank' href="+shareUrl +" class='bi bi-twitter'>Share!</a>";
+shareArea.innerHTML = shareLink;
+}
