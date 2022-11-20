71931a268fb854b25ca66ccfa601e980bb2c52b2
YamanoItsuki <42466955+ituyama@users.noreply.github.com>
2022-11-21 01:56:12 +0900

    add share btn

--- js/index.js
+++ js/index.js
@@ -277,9 +277,9 @@ function ChoiceRandom() {
         MenuList[MenuList.length] = MenuNow.name
         MenuPriceList[MenuPriceList.length] = MenuNow.price
         TotalList[TotalList.length] = MenuNow
- 
+
         price = price + MenuNow.price
-  
+
         res = [MenuList, MenuPriceList]
 
     }
@@ -312,19 +312,19 @@ console.log("答え", ChoiceRandom().length)
 
 function AddHTML() {
     ins = ChoiceRandom();
-    while(ins[ins.length - 1].Total < 970) {
+    while (ins[ins.length - 1].Total < 970) {
         ins = ChoiceRandom();
     }
 
     let strs = "";
     let share_url = "";
-    for (let i = 0; i < ins.length - 1; i++){
+    for (let i = 0; i < ins.length - 1; i++) {
         strs = strs + "<div class='col-md-6'><div class='card'><p class='left'>" + ins[i].name + "</p><p class='prices right'>" + ins[i].price + "円</p></div></div>";
-        share_url = share_url+ins[i].name+"、"+ins[i].price+"円\n";
+        share_url = share_url + ins[i].name + "、" + ins[i].price + "円\n";
     }
-    strs = strs + "<div class='col-md-6'><div class='card'><p class='left'>合計" + ins[ins.length-1].Total +"円</p></div></div>"
+    strs = strs + "<div class='col-md-6'><div class='card'><p class='left'>合計" + ins[ins.length - 1].Total + "円</p></div></div>"
     document.getElementById('content').innerHTML = strs;
-    share_url = share_url + "合計"+ins[ins.length - 1].Total + "円";
+    share_url = share_url + "合計" + ins[ins.length - 1].Total + "円";
     console.log(strs)
     ShareTwitter(share_url)
 }
@@ -333,12 +333,13 @@ function AddHTML() {
 // シェアボタン追加
 
 function ShareTwitter(share) {
-var shareUrl  = 'https://twitter.com/intent/tweet';
-shareUrl += '?text='+encodeURIComponent("すき家1000円ガチャを回したよ!\n"+share);
-shareUrl += '&url='+encodeURIComponent('\nhttps://ituyama.com/SukiyaRandom\n');
-shareUrl+='&hashtags=すき家1000円ガチャ'
+    var shareUrl = 'https://twitter.com/intent/tweet';
+    shareUrl += '?text=' + encodeURIComponent("すき家1000円ガチャを回したよ!\n" + share);
+    shareUrl += '&url=' + encodeURIComponent('\nhttps://ituyama.com/SukiyaRandom\n');
+    shareUrl += '&hashtags=すき家1000円ガチャ'
     var shareArea = document.getElementById('twitter-share');
+
+    var shareLink = "<a target='_blank' href=" + shareUrl + " class='bi  bi-twitter twitter-share'>結果を共有する！</a>";
     
-var shareLink = "<a target='_blank' href="+shareUrl +" class='bi  bi-twitter twitter-share'>Share!</a>";
-shareArea.innerHTML = shareLink;
+    shareArea.innerHTML = shareLink;
 }
